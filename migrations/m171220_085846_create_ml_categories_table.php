acdae68ea80986a392a5d203ebcb96af42c12155
Lix <xiao.liunit@gmail.com>
2017-12-20 20:08:14 +0800

    change categories

--- migrations/m171220_085846_create_ml_categories_table.php
+++ migrations/m171220_085846_create_ml_categories_table.php
@@ -14,12 +14,12 @@ class m171220_085846_create_ml_categories_table extends Migration
     {
         $this->createTable('ml_categories', [
             'id' => $this->primaryKey(),
-            'uid' => $this->integer()->notNull()->defaultValue(0),
-            'name' => $this->string()->notNull()->defaultValue(''),
-            'type_id' => $this->integer()->notNull(),
-            'total_up' => $this->integer()->notNull()->defaultValue(0),
-            'created_time' => $this->dateTime(),
-            'updated_time' => $this->dateTime(),
+            'uid' => $this->integer()->notNull()->defaultValue(0)->comment('用户id'),
+            'name' => $this->string()->notNull()->defaultValue('')->comment('清单 类目名称'),
+            'type_id' => $this->integer()->notNull()->comment('类型id, 为使用icon做区分'),
+            'total_up' => $this->integer()->notNull()->defaultValue(0)->comment('清单总金额'),
+            'created_time' => $this->dateTime()->comment('创建时间'),
+            'updated_time' => $this->dateTime()->comment('更新时间'),
         ]);
     }
 
