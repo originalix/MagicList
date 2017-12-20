a2b822f4ae46f93d2c7a7783a8cddbb51433ed19
Lix <xiao.liunit@gmail.com>
2017-12-20 16:05:41 +0800

    add resuful parse

--- config/db.php
+++ config/db.php
@@ -2,13 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=magiclist',
     'username' => 'root',
-    'password' => '',
+    'password' => '123456',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'tablePrefix' => 'ml_',
 ];
