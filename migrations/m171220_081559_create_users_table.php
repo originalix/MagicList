64543bb6f638fe41a255276ad04c25334c497652
Lix <xiao.liunit@gmail.com>
2017-12-20 16:39:51 +0800

    create users table

--- migrations/m171220_081559_create_users_table.php
+++ migrations/m171220_081559_create_users_table.php
@@ -14,6 +14,12 @@ class m171220_081559_create_users_table extends Migration
     {
         $this->createTable('users', [
             'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull(),
+            'password' => $this->string()->notNull(),
+            'access_token' => $this->string(),
+            'qq_account' => $this->string(),
+            'created_time' => $this->dateTime(),
+            'updated_time' => $this->dateTime(),
         ]);
     }
 
