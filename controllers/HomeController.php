edeac63ed92da405c8045d872743ff2e60478907
Lix <xiao.liunit@gmail.com>
2017-12-21 11:30:28 +0800

    add index php

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -3,12 +3,15 @@
 namespace app\controllers;
 
 use Yii;
+use yii\web\Controller;
+use yii\web\Response;
 use app\models\User;
 
-class HomeController
+class HomeController extends Controller
 {
     public function actionIndex()
     {
+        $this->layout = false;
         return $this->render('index');
     }
 }
