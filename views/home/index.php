d3519fa7a1609b37f42d467c6398c206fe8793b1
Lix <xiao.liunit@gmail.com>
2017-12-21 17:03:05 +0800

    test heade

--- views/home/index.php
+++ views/home/index.php
@@ -23,29 +23,10 @@
     <section class="home-index box">
         <header class="home-index-bar home-index-bar-nav">
             <img class="home-index-portrait" src="http://cms-bucket.nosdn.127.net/e76028c29cb64592868934c3b2e69f7720170516111348.jpeg?imageView&thumbnail=550x0" alt="">
-            <span>username</span>
+            <span class="home-header-username">username</span>
             <i class="iconfont">&#xe62d;</i>
         </header>
         <div class="home-index-main">
-            <div class="box1">
-                <p>1</p>
-            </div>
-            <div class="box2">
-                <p>2</p>
-            </div>
-            <div class="box3">
-                <p>3</p>
-            </div>
-
-            <div class="box1">
-                <p>4</p>
-            </div>
-            <div class="box2">
-                <p>5</p>
-            </div>
-            <div class="box3">
-                <p>6</p>
-            </div>
 
         </div>
     </section>
