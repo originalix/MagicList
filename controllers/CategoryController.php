56d21710c34e9965a66627b6aa8299349df114e0
Lix <xiao.liunit@gmail.com>
2017-12-24 09:22:41 +0800

    create category controller

---
+++ controllers/CategoryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Response;
+use app\models\User;
+use app\models\Categories;
+
+class CategoryController extends BaseController
+{
+    
+}
