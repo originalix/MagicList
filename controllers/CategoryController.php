920d59717f9a4b026d3ce54018c84b35a069ff9f
Lix <xiao.liunit@gmail.com>
2017-12-24 09:42:38 +0800

    create index php

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -5,12 +5,14 @@ namespace app\controllers;
 use Yii;
 use yii\web\Response;
 use app\models\User;
+use app\models\Products;
 use app\models\Categories;
 
 class CategoryController extends BaseController
 {
     public function actionIndex($id)
     {
-        
+        $models = Products::find()->where(['category_id' => $id])->orderBy('created_at')->all();
+        return $this->render('index', ['products' => $models]);
     }
 }
