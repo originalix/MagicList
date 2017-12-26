534034ec8cb0f7b63f819ad5a90c1db7bb5eefd7
Lix <xiao.liunit@gmail.com>
2017-12-26 11:45:14 +0800

    add category js

--- views/category/index.php
+++ views/category/index.php
@@ -75,7 +75,7 @@
                 <!-- 编辑cell -->
                 <div class="list-content hide">
                         <!-- 尚未编辑 -->
-                        <a href="#" class="list-link wait-edit" onclick="changeEditState()">
+                        <a href="#" class="list-link wait-edit" onclick="category.changeEditState()">
                             <div class="list-icon-block">
                                 <i class="list-icon iconfont">&#xe703;</i>
                             </div>
@@ -88,7 +88,7 @@
                         <!-- 开始编辑 -->
                         <a href="#" class="list-link editing">
                             <div class="list-icon-edit-block">
-                                <i class="list-left-icon iconfont" onclick="changeEditState()">&#xe625;</i>
+                                <i class="list-left-icon iconfont" onclick="category.changeEditState()">&#xe625;</i>
 
                                 <i class="list-right-icon iconfont" onclick="submitForm()">&#xe627;</i>
 
@@ -123,70 +123,71 @@
     </div>
 </body>
 <script src="assets/js/jquery-3.2.1.js"></script>
+<script src="assets/js/category.js"></script>
+<script src="assets/js/home.js"></script>
 <script>
-    function changeEditState()
-    {
-        if ($(".editing").css("display") === 'none') {
-            console.log('开始编辑');
-            $(".wait-edit").css("display", "none");
-            $(".editing").css("display", 'block');
-        } else {
-            console.log('关闭编辑');
-            $(".wait-edit").css("display", "block");
-            $(".editing").css("display", 'none');
-        }
-    }
-
-    function refreshData(data)
-    {
-        if (data.length <= 0) {
-            return;
-        }
-        var html = "";
-        for (var i = 0; i < data.length; i++) {
-            html += "<div class='list-content hide' id= " + 
-            data[i].id +
-            "<a href='#' class='list-link'> <div class='product-list-icon-block'> <i class='product-left-icon iconfont'>&#xe67d;</i> <i class='product-center-icon iconfont'>&#xe636;</i> <i class='product-right-icon iconfont'>&#xe603;</i> </div> <div class='list-title-block product-list-title-block'> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe693;</i> <p class='product-list-title product-name'>" +
-            data[i].name + 
-            "</p> </div> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe6da;</i> <p class='product-list-title product-price'> ￥" +
-            data[i].price  + 
-            "</p> </div> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe6f1;</i> <p class='product-list-title product-count'>" + 
-             data[i].count + 
-             "</p> </div> </div> </a> </div>";
-            $(".category-list").html(html);
-        }
-    }
-
-    function submitForm()
-    {
-        var params = {
-            "name": $("#name").val(),
-            "price": $("#price").val(),
-            "count": $("#count").val(),
-            "category_id" : <?php echo $category->id; ?>,
-        };
-
-        // console.log(params);
-
-        $.ajax({
-            type: "POST",
-            url: "",
-            data: params,
-            dataType: "json",
-            success: function(response) {
-                var code = response.code;
-                var msg = response.msg;
-                if (code == 200) {
-                    console.log(response.data);
-                    changeEditState();
-                    // location.reload();
-                    refreshData(response.data);
-                } else {
-                    console.log('code = ' + code + '; msg = ' + msg);
-                }
-            }
-        });
-    }
-
+    var category = Category.createNew();
+    console.log(category);
+    var home = Home.createNew();
+    console.log(home);
+    // function changeEditState()
+    // {
+    //     if ($(".editing").css("display") === 'none') {
+    //         console.log('开始编辑');
+    //         $(".wait-edit").css("display", "none");
+    //         $(".editing").css("display", 'block');
+    //     } else {
+    //         console.log('关闭编辑');
+    //         $(".wait-edit").css("display", "block");
+    //         $(".editing").css("display", 'none');
+    //     }
+    // }
+    // function refreshData(data)
+    // {
+    //     if (data.length <= 0) {
+    //         return;
+    //     }
+    //     var html = "";
+    //     for (var i = 0; i < data.length; i++) {
+    //         html += "<div class='list-content hide' id= " + 
+    //         data[i].id +
+    //         "<a href='#' class='list-link'> <div class='product-list-icon-block'> <i class='product-left-icon iconfont'>&#xe67d;</i> <i class='product-center-icon iconfont'>&#xe636;</i> <i class='product-right-icon iconfont'>&#xe603;</i> </div> <div class='list-title-block product-list-title-block'> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe693;</i> <p class='product-list-title product-name'>" +
+    //         data[i].name + 
+    //         "</p> </div> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe6da;</i> <p class='product-list-title product-price'> ￥" +
+    //         data[i].price  + 
+    //         "</p> </div> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe6f1;</i> <p class='product-list-title product-count'>" + 
+    //          data[i].count + 
+    //          "</p> </div> </div> </a> </div>";
+    //         $(".category-list").html(html);
+    //     }
+    // }
+    // function submitForm()
+    // {
+    //     var params = {
+    //         "name": $("#name").val(),
+    //         "price": $("#price").val(),
+    //         "count": $("#count").val(),
+    //         "category_id" : <?php echo $category->id; ?>,
+    //     };
+    //     // console.log(params);
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "",
+    //         data: params,
+    //         dataType: "json",
+    //         success: function(response) {
+    //             var code = response.code;
+    //             var msg = response.msg;
+    //             if (code == 200) {
+    //                 console.log(response.data);
+    //                 changeEditState();
+    //                 // location.reload();
+    //                 refreshData(response.data);
+    //             } else {
+    //                 console.log('code = ' + code + '; msg = ' + msg);
+    //             }
+    //         }
+    //     });
+    // }
 </script>
 </html>
--- web/assets/js/category.js
+++ web/assets/js/category.js
@@ -1,61 +1,54 @@
 var Category = {
-    createNew: function ()
-    {
+    createNew: function () {
         var category = {};
 
-        category.changeEditState = function ()
-        {
-            function changeEditState()
-            {
-                if ($(".editing").css("display") === 'none') {
-                    console.log('开始编辑');
-                    $(".wait-edit").css("display", "none");
-                    $(".editing").css("display", 'block');
-                } else {
-                    console.log('关闭编辑');
-                    $(".wait-edit").css("display", "block");
-                    $(".editing").css("display", 'none');
-                }
+        category.changeEditState = function () {
+            if ($(".editing").css("display") === 'none') {
+                console.log('开始编辑');
+                $(".wait-edit").css("display", "none");
+                $(".editing").css("display", 'block');
+            } else {
+                console.log('关闭编辑');
+                $(".wait-edit").css("display", "block");
+                $(".editing").css("display", 'none');
             }
         }
 
-        category.refreshData = function(data)
-        {
+        category.refreshData = function (data) {
             if (data.length <= 0) {
                 return;
             }
             var html = "";
             for (var i = 0; i < data.length; i++) {
-                html += "<div class='list-content hide' id= " + 
-                data[i].id +
-                "<a href='#' class='list-link'> <div class='product-list-icon-block'> <i class='product-left-icon iconfont'>&#xe67d;</i> <i class='product-center-icon iconfont'>&#xe636;</i> <i class='product-right-icon iconfont'>&#xe603;</i> </div> <div class='list-title-block product-list-title-block'> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe693;</i> <p class='product-list-title product-name'>" +
-                data[i].name + 
-                "</p> </div> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe6da;</i> <p class='product-list-title product-price'> ￥" +
-                data[i].price  + 
-                "</p> </div> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe6f1;</i> <p class='product-list-title product-count'>" + 
-                 data[i].count + 
-                 "</p> </div> </div> </a> </div>";
+                html += "<div class='list-content hide' id= " +
+                    data[i].id +
+                    "<a href='#' class='list-link'> <ddiv class='product-list-icon-block'> <i class='product-left-icon iconfont'>&#xe67d;</i> <i class='product-center-icon iconfont'>&#xe636;</i> <i class='product-right-icon iconfont'>&#xe603;</i> </ddiv> <div class='list-title-block product-list-title-block'> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe693;</i> <p class='product-list-title product-name'>" +
+                    data[i].name +
+                    "</p> </div> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe6da;</i> <p class='product-list-title product-price'> ￥" +
+                    data[i].price +
+                    "</p> </div> <div class='product-list-seprate'> <i class='list-name-icon iconfont'>&#xe6f1;</i> <p class='product-list-title product-count'>" +
+                    data[i].count +
+                    "</p> </div> </div> </a> </div>";
                 $(".category-list").html(html);
             }
         }
 
-        category.submitForm = function(id)
-        {
+        category.submitForm = function (id) {
             var params = {
                 "name": $("#name").val(),
                 "price": $("#price").val(),
                 "count": $("#count").val(),
-                "category_id" : id,
+                "category_id": id,
             };
-    
+
             // console.log(params);
-    
+
             $.ajax({
                 type: "POST",
                 url: "",
                 data: params,
                 dataType: "json",
-                success: function(response) {
+                success: function (response) {
                     var code = response.code;
                     var msg = response.msg;
                     if (code == 200) {
@@ -69,5 +62,7 @@ var Category = {
                 }
             });
         }
+
+        return category;
     }
 }
