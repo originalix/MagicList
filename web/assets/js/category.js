2074e571785de126f942b009dc6abb64d05de014
Lix <xiao.liunit@gmail.com>
2017-12-30 20:32:47 +0800

    增加编辑样式

--- web/assets/js/category.js
+++ web/assets/js/category.js
@@ -138,7 +138,11 @@ var Category = {
         }
 
         category.editCell = function (id) {
-            console.log('edit cell');
+            console.log(id);
+            var cellId = "#list-content" + id;
+            $(cellId).html("<!-- 开始编辑 --> <div href='#' class='list-link'> <div class='list-icon-edit-block'> <i class='list-left-icon iconfont' onclick='category.changeEditState()'>&#xe625;</i> <i class='list-right-icon iconfont' onclick='category.submitForm(" +
+            id +
+            ")'>&#xe627;</i> </div> <div class='add-form'> <form id='form' method='post' class='form-content'> <div class='input-control'> <i class='form-edit-icon iconfont'>&#xe693;</i> <input type='text' name='name' id='name' class='category-input' placeholder='单品名称'> <hr class='form-separated'></hr> </div> <div class='input-control'> <i class='form-edit-icon iconfont'>&#xe6da;</i> <input type='text' name='price' id='price' class='category-input' placeholder='单品单价'> <hr class='form-separated'></hr> </div> <div class='input-control'> <i class='form-edit-icon iconfont'>&#xe6f1;</i> <input type='text' name='count' id='count' class='category-input' placeholder='购买数量'> <hr class='form-separated'></hr> </div> <input type='hidden' name='_csrf' value='<?=Yii::$app->request->getCsrfToken()?>' /> </form> </div> </div> </div>");
         }
 
         return category;
